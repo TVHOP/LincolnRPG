65c3a345b0d48d2e8d9a3f92585e95f6beea36be
Melanson4202097 <4202097@sps-student.sk.ca>
2021-11-23 12:01:49 -0600

    need to figure out P5.Play
    urgently

--- sketch.js
+++ sketch.js
@@ -1,191 +1,233 @@
-let grid = [];
-let gridSize = 100;
-let cellWidth, cellHeight;
-
-//images
-let leafyGrass;
-let houseRoof;
-let stonePath;
-let woodFloor;
-let defaultLincoln;
-
-
-let level1, level2, level3;
-let state;
-
-let playerX = 1;
-let playerY = 0;
-
-//loads images and maps
-function preload() {
-  // level1 = loadJSON("assets/ANewDay.json");
-  // level2 = loadJSON("assets/TheGloriousLegion.json");
-  
-
-
-  leafyGrass = loadImage("assets/leafygrass.png");
-  stonePath = loadImage("assets/stonepath.png");
-  woodFloor = loadImage("assets/woodfloor.png");
-  houseRoof = loadImage("assets/houseroof.png");
-  defaultLincoln = loadImage("assets/defaultLincoln.jpg");
-  
-}
-
-
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  grid = createRandom2DArray(gridSize, gridSize);
-  // grid = level1;
-  // state = "level1";
-
-
-
-  cellWidth = width/gridSize;
-  cellHeight = height/gridSize;
-
-  grid[playerY][playerX] = 600;
-
-  
-
-  
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
 }
 
 function draw() {
-  background(220);
-  displayGrid();
-//   levelSwap();
+  background(255,255,255);  
+  drawSprites();
 }
 
+// let grid = [];
+// let gridSize = 100;
+// let cellWidth, cellHeight;
 
-function createRandom2DArray(rows,cols) {
-  let board =  [];
-  for (let y=0; y<rows; y++) {
-    board.push([]);
-    for (let x=0; x<cols; x++) {
-      if (random(100) < 50) {
-        board[y].push(0);
-      }
-      else {
-        board[y].push(1);
-      }
-    }
-  }
-  return board;
-}
+// //images
+// let leafyGrass;
+// let houseRoof;
+// let stonePath;
+// let woodFloor;
+// let defaultLincoln;
+// let standingGuy;
+// let walkingGuy;
 
-//movement controls
-function keyPressed() {
-  // if (key === "e") {
-  //   grid = createEmpty2DArray(gridSize,gridSize);
-  // }
-  if (key === "r") {
-    grid=createRandom2DArray(gridSize,gridSize);
-  }
-  if (key === "s") {
-    tryToMoveTo(playerX,playerY+1);
-  }
-  if (key === "w") {
-    tryToMoveTo(playerX,playerY-1);
-  }
-  if (key === "a") {
-    tryToMoveTo(playerX-1,playerY);
-  }
-  if (key === "d") {
-    tryToMoveTo(playerX+1,playerY);
-  }
-}
+// let sceneW;
+// let sceneH;
+// let marginW;
+// let marginH;
 
-//attempts to move character
-function tryToMoveTo(newX, newY) {
-  if (newX >= 0 && newY >= 0 && newX < gridSize && newY < gridSize) {
-
-    console.log(newX, newY);
-    
-    if (grid[newY][newX] === 0) {
-      grid[playerY][playerX] = 0;
-      playerX = newX;
-      playerY = newY;
-      grid[playerY][playerX] = 600;
-    }
-    if (grid[newY][newX] === 3) {
-      grid[playerY][playerX] = 3;
-      playerX = newX;
-      playerY = newY;
-      grid[playerY][playerX] = 600;
-    }
-  }
-}
+// let dude;
 
 
-//rotates between tiles on mouse click
-function mousePressed()  {
-  let cellX = Math.floor(mouseX/cellWidth);
-  let cellY = Math.floor(mouseY/cellHeight);
 
-  if (grid[cellY][cellX] === 0) {
-    grid[cellY][cellX] = 1;
-  }
-  else if (grid[cellY][cellX] === 1) {
-    grid[cellY][cellX] = 2;
-  }
-  else if (grid[cellY][cellX] === 2) {
-    grid[cellY][cellX] = 3;
-  }
-  else if (grid[cellY][cellX] === 3) {
-    grid[cellY][cellX] = 0;
-  }
+// let level1, level2, level3;
+// let state;
 
-}
+// let playerX = 1;
+// let playerY = 0;
 
-function displayGrid() {
-  for (let y=0; y<gridSize; y++) {
-    for (let x=0; x<gridSize; x++) {
-      if (grid[y][x] === 0) {
-        image(stonePath,x*cellWidth, y*cellHeight, cellWidth, cellHeight);
-      }
-      if (grid[y][x] === 1) {
-        image(leafyGrass,x*cellWidth, y*cellHeight, cellWidth, cellHeight);      
-      }
-      if (grid[y][x] === 2) {
-        image(houseRoof,x*cellWidth, y*cellHeight, cellWidth, cellHeight);
-      }
-      if (grid[y][x] === 3) {
-        image(woodFloor,x*cellWidth, y*cellHeight, cellWidth, cellHeight);
-      }
-      if (grid[y][x] === 600) {
-        image(defaultLincoln,x*cellWidth, y*cellHeight, cellWidth, cellHeight);
-      }
-      
-      
-      strokeWeight(0.1);
-    }
-  }
-}
+// //loads images and maps
+// function preload() {
+//   // level1 = loadJSON("assets/ANewDay.json");
+//   // level2 = loadJSON("assets/TheGloriousLegion.json");
+  
 
-//grid creation
-// function createEmpty2DArray(rows,cols) {
+
+//   leafyGrass = loadImage("assets/leafygrass.png");
+//   stonePath = loadImage("assets/stonepath.png");
+//   woodFloor = loadImage("assets/woodfloor.png");
+//   houseRoof = loadImage("assets/houseroof.png");
+//   defaultLincoln = loadImage("assets/defaultLincoln.jpg");
+  
+//   standingGuy = loadImage("assets/testman.png");
+//   walkingGuy = loadImage("assets/testman2.png");
+// }
+
+// function cameraboys() {
+//   camera.on();
+//   camera.zoom = 1.5;
+//   camera.position.x = dude.position.x;
+//   camera.position.y = dude.position.y;
+// }
+
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+//   grid = createRandom2DArray(gridSize, gridSize);
+//   // grid = level1;
+//   // state = "level1";
+
+
+
+//   cellWidth = width/gridSize;
+//   cellHeight = height/gridSize;
+
+//   grid[playerY][playerX] = 600;
+
+//   sceneW = width + width*0.2;
+//   sceneH = height + height*0.2;
+
+//   marginW = width*0.4;
+//   marginH = height*0.4;
+// }
+
+// function draw() {
+//   background(220);
+//   displayGrid();
+//   //   levelSwap();
+//   cameraboys();
+
+//   drawSprites(dude);
+// }
+
+// function createDude(x,y,w,h) {
+//   dude = createWriter(x,y,w,h);
+//   dude.addAnimation("walking", standingGuy, walkingGuy);
+// }
+
+// function moveDude() {
+//   if (keyIsDown("W")) {
+//     dude.changeAnimation("walking");
+//   }
+// }
+
+// function createRandom2DArray(rows,cols) {
 //   let board =  [];
 //   for (let y=0; y<rows; y++) {
 //     board.push([]);
 //     for (let x=0; x<cols; x++) {
-//       board[y].push(0);
+//       if (random(100) < 50) {
+//         board[y].push(0);
+//       }
+//       else {
+//         board[y].push(1);
+//       }
 //     }
 //   }
 //   return board;
 // }
 
-// function levelSwap() {
-//   if (state === "level1") {
-//     grid = level1;
-//     if (grid[4][0] === 600) {
-//       state = "level2";
-//     }
+// //movement controls
+// function keyPressed() {
+//   // if (key === "e") {
+//   //   grid = createEmpty2DArray(gridSize,gridSize);
+//   // }
+//   if (key === "r") {
+//     grid=createRandom2DArray(gridSize,gridSize);
 //   }
-//   else if (state === "level2") {
-//     grid = level2;
-//     playerX = 18;
-//     playerY = 9;
-    
+//   if (key === "s") {
+//     tryToMoveTo(playerX,playerY+1);
+//   }
+//   if (key === "w") {
+//     tryToMoveTo(playerX,playerY-1);
+//   }
+//   if (key === "a") {
+//     tryToMoveTo(playerX-1,playerY);
+//   }
+//   if (key === "d") {
+//     tryToMoveTo(playerX+1,playerY);
 //   }
 // }
+
+// //attempts to move character
+// function tryToMoveTo(newX, newY) {
+//   if (newX >= 0 && newY >= 0 && newX < gridSize && newY < gridSize) {
+
+//     console.log(newX, newY);
+    
+//     if (grid[newY][newX] === 0) {
+//       grid[playerY][playerX] = 0;
+//       playerX = newX;
+//       playerY = newY;
+//       grid[playerY][playerX] = 600;
+//     }
+//     if (grid[newY][newX] === 3) {
+//       grid[playerY][playerX] = 3;
+//       playerX = newX;
+//       playerY = newY;
+//       grid[playerY][playerX] = 600;
+//     }
+//   }
+// }
+
+
+// //rotates between tiles on mouse click
+// function mousePressed()  {
+//   let cellX = Math.floor(mouseX/cellWidth);
+//   let cellY = Math.floor(mouseY/cellHeight);
+
+//   if (grid[cellY][cellX] === 0) {
+//     grid[cellY][cellX] = 1;
+//   }
+//   else if (grid[cellY][cellX] === 1) {
+//     grid[cellY][cellX] = 2;
+//   }
+//   else if (grid[cellY][cellX] === 2) {
+//     grid[cellY][cellX] = 3;
+//   }
+//   else if (grid[cellY][cellX] === 3) {
+//     grid[cellY][cellX] = 0;
+//   }
+
+// }
+
+// function displayGrid() {
+//   for (let y=0; y<gridSize; y++) {
+//     for (let x=0; x<gridSize; x++) {
+//       if (grid[y][x] === 0) {
+//         image(stonePath,x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+//       }
+//       if (grid[y][x] === 1) {
+//         image(leafyGrass,x*cellWidth, y*cellHeight, cellWidth, cellHeight);      
+//       }
+//       if (grid[y][x] === 2) {
+//         image(houseRoof,x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+//       }
+//       if (grid[y][x] === 3) {
+//         image(woodFloor,x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+//       }
+//       if (grid[y][x] === 600) {
+//         image(defaultLincoln,x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+//       }
+      
+      
+//       strokeWeight(0.1);
+//     }
+//   }
+// }
+
+// //grid creation
+// // function createEmpty2DArray(rows,cols) {
+// //   let board =  [];
+// //   for (let y=0; y<rows; y++) {
+// //     board.push([]);
+// //     for (let x=0; x<cols; x++) {
+// //       board[y].push(0);
+// //     }
+// //   }
+// //   return board;
+// // }
+
+// // function levelSwap() {
+// //   if (state === "level1") {
+// //     grid = level1;
+// //     if (grid[4][0] === 600) {
+// //       state = "level2";
+// //     }
+// //   }
+// //   else if (state === "level2") {
+// //     grid = level2;
+// //     playerX = 18;
+// //     playerY = 9;
+    
+// //   }
+// // }
