e0268acc8056bcf5604497ea787017d0f31557fb
TVHOP <74019075+TVHOP@users.noreply.github.com>
2022-01-25 12:07:44 -0600

    anger
    in a final push of desperation, he creates a sad looking title screen that he will finish later

--- sketch.js
+++ sketch.js
@@ -15,6 +15,8 @@ let bullet;
 let laserbeam;
 let temp1;
 
+let state = "SCREEN";
+
 let sceneW;
 let sceneH;
 
@@ -22,10 +24,14 @@ let marginW;
 let marginH;
 
 let boss_size;
+
+let bgButton;
+let shapeButton;
+let bgColor = "limegreen";
+let isShapeDisplayed = false;
 //--------------------------------------------------------------------------------------------------------//
 function preload() {
   boss_size = loadImage("assets/400x400.png");
-  bulletsImg = loadImage("assets/soy.png");
   laserbeam = loadImage("assets/powpow.png");
 
 }
@@ -33,9 +39,11 @@ function preload() {
 //Setup Function
 function setup() {
   createCanvas(windowWidth,windowHeight);
-  createBenedict();
-  createZES();
   document.addEventListener("contextmenu", event => event.preventDefault());
+
+  if (state === "PLAY") {
+    createBenedict();
+    createZES();
 
   let sceneW = width + width*0.2;
   let sceneH = height + height*0.2;
@@ -47,6 +55,12 @@ function setup() {
 
   benedict.position.x = width/2;
   benedict.position.y = height/2;
+
+  bgButton = new Button(100, 300, 600, 150,"red","blue");
+  shapeButton = new Button(100,100,600,150,"yellow","green");
+  }
+
+  
 
   
 }
@@ -60,7 +74,6 @@ function draw() {
   
   borderlands();
 
-  // drawSprite(playerBullets);
   drawSprite(benedict);
   drawSprite(ZES);
   
@@ -72,6 +85,16 @@ function draw() {
 
   
   handleBullets();
+
+  startScreen();
+
+  bgButton.display();
+  shapeButton.display();
+
+  if (isShapeDisplayed) {
+    fill("black");
+    circle(600,200,100);
+  }
 
 
   
@@ -120,77 +143,85 @@ function testGun() {
 //The code that allows Benedict to move around. Standard WASD + Shift.
 function WASDcontrol() {
   //Up Movement, and Sprint Up Movement
-  if (keyIsDown(87)) {
-    if (keyIsDown(16)) {
+  if (state === "PLAY") {
+    if (keyIsDown(87)) {
+      if (keyIsDown(16)) {
+        benedict.changeAnimation("moveforward");
+        benedict.position.y -= 11;
+        console.log("Wsprint");
+      }
       benedict.changeAnimation("moveforward");
-      benedict.position.y -= 11;
-      console.log("Wsprint");
+      benedict.position.y -= 7;
+      console.log("w");
     }
-    benedict.changeAnimation("moveforward");
-    benedict.position.y -= 7;
-    console.log("w");
-  }
-  else {
-    benedict.changeAnimation("standing");
-  }
-  // Right Movement, and Sprint Right Movement
-  if (keyIsDown(65)) {
-    if (keyIsDown(16)) {
+    else {
+      benedict.changeAnimation("standing");
+    }
+    // Right Movement, and Sprint Right Movement
+    if (keyIsDown(65)) {
+      if (keyIsDown(16)) {
+        benedict.changeAnimation("moveleft");
+        benedict.position.x -= 11;
+        console.log("Asprint");
+      }
       benedict.changeAnimation("moveleft");
-      benedict.position.x -= 11;
-      console.log("Asprint");
+      benedict.position.x -= 7;
+      console.log("a");
     }
-    benedict.changeAnimation("moveleft");
-    benedict.position.x -= 7;
-    console.log("a");
-  }
-  // Down Movement, and Sprint Down Movement
-  if (keyIsDown(83)) {
-    if (keyIsDown(16)) {
+    // Down Movement, and Sprint Down Movement
+    if (keyIsDown(83)) {
+      if (keyIsDown(16)) {
+        benedict.changeAnimation("movebackward");
+        benedict.position.y += 11;
+        console.log("Ssprint");
+      }
       benedict.changeAnimation("movebackward");
-      benedict.position.y += 11;
-      console.log("Ssprint");
+      benedict.position.y += 7;
+      console.log("s");
     }
-    benedict.changeAnimation("movebackward");
-    benedict.position.y += 7;
-    console.log("s");
+    // Left Movement, and Sprint Left Movement
+    if (keyIsDown(68)) {
+      benedict.changeAnimation("moveright");
+      benedict.position.x += 7;
+      console.log("d");
+    }
+    if (keyIsDown(16) && keyIsDown(68)) {
+      benedict.changeAnimation("moveright");
+      benedict.position.x += 11;
+      console.log("Dsprint");
+    }
+    ZES.changeAnimation("Idle");
   }
-  // Left Movement, and Sprint Left Movement
-  if (keyIsDown(68)) {
-    benedict.changeAnimation("moveright");
-    benedict.position.x += 7;
-    console.log("d");
-  }
-  if (keyIsDown(16) && keyIsDown(68)) {
-    benedict.changeAnimation("moveright");
-    benedict.position.x += 11;
-    console.log("Dsprint");
-  }
-  ZES.changeAnimation("Idle");
 }
 //---------------------------------------------------------------------------------------------------------//
 //Defines the borders of the play area and does not allow Benedict to go past it. Named after a popular game I didn't enjoy.
 function borderlands() {
-  if(benedict.position.x < 15) {
-    benedict.position.x = 15;
-  }
-  if(benedict.position.y < 37) {
-    benedict.position.y = 37;
-  }
-  if(benedict.position.x > 1590) {
-    benedict.position.x = 1590;
-  }
-  if(benedict.position.y > 755) {
-    benedict.position.y = 755;
+  if (state === "PLAY") {
+    if(benedict.position.x < 15) {
+      benedict.position.x = 15;
+    }
+    if(benedict.position.y < 37) {
+      benedict.position.y = 37;
+    }
+    if(benedict.position.x > 1590) {
+      benedict.position.x = 1590;
+    }
+    if(benedict.position.y > 755) {
+      benedict.position.y = 755;
+    }
   }
 }
 //---------------------------------------------------------------------------------------------------------//
 function collideControl() {
-  benedict.overlap(ZES, bounceControl);
+  if (state === "PLAY"){
+    benedict.overlap(ZES, bounceControl);
+  }
 }
 
 function bounceControl(benedict, ZES) {
-  benedict.bounce(ZES);
+  if (state === "PLAY"){
+    benedict.bounce(ZES); 
+  }
 }
 //---------------------------------------------------------------------------------------------------------//
 let bullets = [];
@@ -227,6 +258,50 @@ function spawnBullet() {
 }
 
 function mousePressed() {
-  spawnBullet();
+  if (state === "PLAY") {
+    spawnBullet();
+
+  if (bgButton.isPointInButton(mouseX,mouseY)) {
+    bgColor = "darkgreen";
+  }
+  if (shapeButton.isPointInButton(mouseX,mouseY)) {
+    isShapeDisplayed = !isShapeDisplayed;
+  }
+  }
 }
+
+function startScreen() {
+  if (state === "SCREEN") {
+    background("black");
+    textAlign(CENTER);
+    textSize(100);
+    fill("white");
+    text("testing", windowWidth/2, 150);
+  }
+}
+class Button {
+  constructor(x,y,buttonWidth,buttonHeight,hoverColor,notHovercolor) {
+    this.x = x;
+    this.y = y;
+    this.width = buttonWidth;
+    this.height = buttonHeight;
+    this.notHovercolor = notHovercolor;
+    this.hoverColor = hoverColor;
+  }
+  display() {
+    if (this.isPointInButton(mouseX,mouseY)) {
+      fill(this.hoverColor);
+    }
+    else {
+      fill(this.notHovercolor);
+    }
+    rect(this.x, this.y, this.width, this.height);
+    }
+  
+  isPointInButton(x,y) {
+    return x >= this.x && x <= this.x + this.width && y >= this.y && y <= this.y + this.height;
+  }
+}
+
+
 
